42e1e267d8651ba588ca5a36b586c3bdfcdc426c
gustavoromerocl <gandresrp@gmail.com>
2025-01-05 17:42:36 -0300

    feat: create msal service

---
+++ src/app/services/msal/msal.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { PublicClientApplication, Configuration, InteractionType, InteractionRequiredAuthError } from '@azure/msal-browser';
+import { Store } from '@ngrx/store';
+import { login } from '../../store/session/session.reducer';
+
+@Injectable({ providedIn: 'root' })
+export class MsalService {
+  private msalInstance: PublicClientApplication;
+
+  constructor(private store: Store) {
+    const config: Configuration = {
+      auth: {
+        clientId: '858b4bc0-7115-4192-a217-5de9275c3ab1', // Reemplaza con el ID de tu aplicación registrada en Azure AD.
+        authority: 'https://login.microsoftonline.com/6594e534-4a19-4085-a8e1-411ef99af367', // Reemplaza con tu tenant ID.
+        redirectUri: window.location.origin
+      }
+    };
+
+    // Call initialize here
+    this.msalInstance = new PublicClientApplication(config);
+    this.msalInstance.initialize().catch(error => {
+        console.error("Error initializing MSAL instance:", error);
+    });
+  }
+
+  login() {
+    this.msalInstance.loginPopup({
+      scopes: ['user.read'], // Solicita los permisos necesarios
+      prompt: 'select_account' // Fuerza al usuario a seleccionar una cuenta
+    })
+    .then(response => {
+      const { account, accessToken } = response;
+      localStorage.setItem('token', accessToken);
+      this.store.dispatch(login({ user: { 
+        id: account.localAccountId, 
+        name: account.name, 
+        email: account.username, 
+        role: 'ROLE_USER' //Establecer cuando se implemente en MS AD
+      }}));
+
+      // this.router.navigate(['/home']);
+    })
+    .catch(error => {
+      console.error('Error al iniciar sesión:', error);
+      // Manejar el error de forma más específica
+      if (error instanceof InteractionRequiredAuthError) {
+        // El usuario debe interactuar (por ejemplo, iniciar sesión)
+        console.log('El usuario debe iniciar sesión');
+      }
+    });
+  }
+}
